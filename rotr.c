3f83b72ba99a86c7b3ddb658d6e85c72258126bb
MarwaAbuEssa <meromeroo@gmail.com>
2023-09-22 01:38:58 +0300

    rotate function

---
+++ rotr.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+void _rotr(stack_t **stack, unsigned int line_number);
+
+/**
+ * _rotr - reverse rotate.
+ * @stack: stack pointer.
+ * @line_number: line number.
+ */
+void _rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *bottom;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	bottom->prev->next = NULL;
+	(*stack)->next = bottom;
+	bottom->prev = *stack;
+	bottom->next = top;
+	top->prev = bottom;
+
+	(void)line_number;
+}
+
